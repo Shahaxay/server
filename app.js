86196fa97d0d8787a55a326a633027966abc892f
Akshayshah47 <removeit61@gmail.com>
2023-06-10 15:00:51 +0530

    /home, /about, /node route to server

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const http=require('http');
+//creating server
 const server=http.createServer((req,res)=>{
-    console.log(req);
+    if(req.url=="/home"){
+        res.write('<html><body><h1>Welcome Home</h1></body></html>');
+        console.log("home");
+    }else if(req.url=="/about"){
+        res.write("Welcome to About Us page");
+        
+    }else if(req.url=="/node"){
+        res.write("Welcome to my Node Js project");
+
+    }
 });
+
+//listening to the port
 server.listen(4000,()=>console.log("Akshay Kumar Sah"));
