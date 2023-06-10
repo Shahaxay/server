ab1628f13c167861935fc95967bb42c6ef971a89
Akshayshah47 <removeit61@gmail.com>
2023-06-10 15:10:26 +0530

    response write only html code so modified text to html content of /about and /node

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const http=require('http');
 //creating server
 const server=http.createServer((req,res)=>{
     if(req.url=="/home"){
-        res.write('<html><body><h1>Welcome Home</h1></body></html>');
-        console.log("home");
+        res.write("<p>Welcome Home</p>");
     }else if(req.url=="/about"){
-        res.write("Welcome to About Us page");
+        res.write("<p>Welcome to About Us page</p>");
         
     }else if(req.url=="/node"){
-        res.write("Welcome to my Node Js project");
+        res.write("<p>Welcome to my Node Js project</p>");
 
-    }
+    }else console.log(req.url);
 });
 
 //listening to the port
